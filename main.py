92b5f2e3576b2bb67911be9333476b0b7f1bbc8e
EndzyCodes <135795894+EndzyCodes@users.noreply.github.com>
2024-04-30 00:14:34 +0800

    Refactor GUI

--- main.py
+++ main.py
@@ -1,53 +1,112 @@
 
 
 from Functions import *
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QCheckBox, QPushButton, QGroupBox
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QCheckBox, QPushButton, QGroupBox, QTabWidget
 
 enable_attack = False
+gem_cooldown = False
+
 class ClashOfClansBotGUI(QWidget):
-    global enable_attack
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        x_pos = 30
         self.setFixedSize(500, 500)  # Set the fixed size of the window (width, height)
+        # this is the main window
+        self.tab_widget = QTabWidget(self)
+        self.tab_widget.setGeometry(10, 10, 480, 350) # x, y, width, height
+        self.tab_widget.currentChanged.connect(self.tabChanged)  # Connect tab change event
 
-        # Create a group box for gem settings
-        gem_group_box = QGroupBox("Gem Settings", self)
-        gem_group_box.setGeometry(20, 50, 200, 100)  # Set position and size of the group box
+        #* Create the widgets for each tab
+        self.clan_games_tab = QWidget()
+        self.bb_attack_tab = QWidget()
+        self.main_village_tab = QWidget()
 
-        self.gemCooldownCheckbox = QCheckBox("Gem Challenge Cooldown", self)
-        self.gemCooldownCheckbox.move(x_pos, 70)
-
-        self.clanGamesCheckbox = QCheckBox("Clan Games Mode", self)
-        self.clanGamesCheckbox.move(x_pos, 100)
-
-        self.attackOnlyCheckbox = QCheckBox("Attack Only, No Clan Games", self)
-        self.attackOnlyCheckbox.move(x_pos, 130)
+        #* add/call tabs here to be shown on the GUI
+        self.tab_widget.addTab(self.clan_games_tab, "Clan Games")
+        self.tab_widget.addTab(self.bb_attack_tab, "BB Attack")
+        self.tab_widget.addTab(self.main_village_tab, "Main Village")
 
         self.startButton = QPushButton("Start Bot", self)
-        self.startButton.move(x_pos, 420)
-        self.startButton.resize(120, 40) # Increase the size of the button (width, height)
+        self.startButton.move(30, 420)
+        self.startButton.resize(120, 40)
 
         self.setWindowTitle('Clash of Clans Bot Configuration')
 
+    def tabChanged(self, index):
+        if index == 0:  # Clan Games Tab
+            self.setupClanGamesTab()
+        elif index == 1:  # BB Attack Tab
+            self.setupBBAttackTab()
+        elif index == 2:  # Main Village Tab
+            self.setupMainVillageTab()
+        else:
+            self.removeClanGamesTabWidgets()
+            #  later add here for other tabs
+            # TODO make remove widgets functions for other tabs like the def removeClanGamesTabWidgets(self)
+
+    def setupClanGamesTab(self):
+        self.removeClanGamesTabWidgets()  # Clear previous widgets if any
+
+        x_pos = 10
+        self.gem_group_box = QGroupBox("Clan Games Settings", self.clan_games_tab)
+        self.gem_group_box.setGeometry(15, 15, 200, 100)
+
+        self.gemCooldownCheckbox = QCheckBox("Gem Challenge Cooldown", self.gem_group_box)
+        self.gemCooldownCheckbox.move(x_pos, 20)
+
+        self.clanGamesCheckbox = QCheckBox("Clan Games Mode", self.gem_group_box)
+        self.clanGamesCheckbox.move(x_pos, 45)
+
+        self.attackOnlyCheckbox = QCheckBox("Attack Only, No Clan Games", self.gem_group_box)
+        self.attackOnlyCheckbox.move(x_pos, 70)
+
+    def removeClanGamesTabWidgets(self):
+        if hasattr(self, 'gem_group_box'):
+            self.gem_group_box.setParent(None)
+            self.gem_group_box.deleteLater()
+
+            del self.gemCooldownCheckbox
+            del self.clanGamesCheckbox
+            del self.attackOnlyCheckbox
+
+    def setupBBAttackTab(self):
+        bb_attack_tab = QWidget()
+        bb_attack_layout = QVBoxLayout()
+        bb_attack_tab.setLayout(bb_attack_layout)
+
+        # Add BB Attack tab content here...
+
+        # self.tab_widget.addTab(bb_attack_tab, "BB Attack")
+
+    def setupMainVillageTab(self):
+        main_village_tab = QWidget()
+        main_village_layout = QVBoxLayout()
+        main_village_tab.setLayout(main_village_layout)
+
+        # Add Main Village tab content here...
+
+        # self.tab_widget.addTab(main_village_tab, "Main Village")
+
     def startBot(self):
-        #* Get the state of the checkboxes and store them in variables
         gem_cooldown = self.gemCooldownCheckbox.isChecked()
         participate_in_clan_games = self.clanGamesCheckbox.isChecked()
         attack_only = self.attackOnlyCheckbox.isChecked()
         enable_attack = self.EnableAttackCheckbox.isChecked()
 
         print(f"Starting bot with configurations: Gem Cooldown: {gem_cooldown}, Clan Games: {participate_in_clan_games}, Attack Only: {attack_only}")
+
         # Here you would add the logic to start the bot with these configurations
         #* pass the variables to the functions that need them
         # Example: You might want to call bb_attack_loop() here or modify it to accept parameters based on the GUI settings
-        gem_cooldown=gem_cooldown #* Pass the gem_cooldown state to the purge_challenge function
-        enable_attack=enable_attack
-
+        gem_cooldown = gem_cooldown
+        enable_attack = enable_attack
+        #TODO - do the parameters for the functions that need them
         main()
+
+
 
 
 #* ----- CLICK FUNCTIONS -----
@@ -567,3 +626,4 @@ if __name__ == "__main__":
     ex.show() # show the GUI
     app.exec() # start the application
 
+#TODO - make it save the options you checked in the GUI so you don't have to check it again when you run the script
